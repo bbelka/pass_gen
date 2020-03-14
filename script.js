3e38a575c507aa466e86db74780260b584c83f32
bbtwots <brettbelka@gmail.com>
2020-03-14 11:26:04 -0700

    cleaned js

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
+// function writePassword()
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
 function writePassword() {
+
+//below doesn't seem to work. possibly because of interaction with prompts/alerts.
+document.getElementById('password').value = "";
   //List of alpha characters
   //list of numeric char
   //list of special char
+
   var caps = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -120,5 +117,5 @@ function writePassword() {
 
   //return password to user
 
-  document.getElementById('password').innerText = "Your password is :   " + password
+  document.getElementById('password').value = "Your password is :   " + password
 }
